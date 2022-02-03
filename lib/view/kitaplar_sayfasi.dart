afedf3951aa0fdddc1311e62c7f2a41e4b6907b0
Haluk H <halukh@protonmail.com>
2022-02-03 13:00:11 +0300

    Kitap ekleme bölümü ve kitap modeli oluşruldu

--- lib/view/kitaplar_sayfasi.dart
+++ lib/view/kitaplar_sayfasi.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/view/model/kiap.dart';
 
 class KitaplarSayfasi extends StatelessWidget {
   const KitaplarSayfasi({Key? key}) : super(key: key);
@@ -9,6 +10,55 @@ class KitaplarSayfasi extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Kitaplar Sayfası"),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          _kitapEkle(context);
+        },
+      ),
+    );
+  }
+
+  // KİTAP EKLE
+  void _kitapEkle(BuildContext context) async{
+    String kitapAdi = await _pencereAc(context, "Kitap Adı Giriniz") ?? " ";
+    if(kitapAdi.isNotEmpty){
+      Kitap yeniKitap = Kitap(kitapAdi, DateTime.now());
+    }
+  }
+
+  // PENCERE AÇ
+  Future<String?> _pencereAc(BuildContext context, String baslik) {
+    return showDialog<String>(
+      context: context,
+      builder: (context) {
+        String sonuc = "";
+        return AlertDialog(
+          title: Text(baslik),
+          content: TextField(
+            keyboardType: TextInputType.text,
+            onChanged: (String inputText) {
+              sonuc = inputText;
+            },
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("İptal"),
+              onPressed: () {
+                Navigator.pop(context, "");
+              },
+            ),
+            TextButton(
+              child: const Text(
+                "Onayla",
+              ),
+              onPressed: () {
+                Navigator.pop(context, sonuc.trim());
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
---
+++ lib/view/model/kiap.dart
@@ -0,0 +1,7 @@
+class Kitap {
+  int? id;
+  String isim;
+  DateTime olusturulmaTarihi;
+
+  Kitap(this.isim, this.olusturulmaTarihi);
+}
